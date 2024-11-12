d18a91785fecb86b68d5279a301c96eee87f0a7d
florisb <floris@modernmediahub.nl>
2024-11-12 13:55:54 +0100

    Fix type error

--- src/components/atoms/Link.tsx
+++ src/components/atoms/Link.tsx
@@ -13,12 +13,12 @@ interface LinkComponentProps extends LinkProps {
 }
 
 export const Link: React.FC<PropsWithChildren<LinkComponentProps>> = ({
-    children,
-    skipLocaleHandling,
-    external,
-    ariaLabel,
-    ...rest
-}) => {
+                                                                          children,
+                                                                          skipLocaleHandling,
+                                                                          external,
+                                                                          ariaLabel,
+                                                                          ...rest
+                                                                      }) => {
     const router = useRouter();
     const locale = (rest.locale || router.query.locale || '') as string;
     const channel = (router.query.channel || '') as string;
@@ -26,9 +26,7 @@ export const Link: React.FC<PropsWithChildren<LinkComponentProps>> = ({
     let linkHref = (href || router.asPath) as string;
     if (linkHref.indexOf('http') === 0) skipLocaleHandling = true;
     if (notTranslatedLinks.find(ntl => linkHref.startsWith(ntl))) skipLocaleHandling = true;
-    // if (locale && !skipLocaleHandling && locale !== 'en') {
-    //     linkHref = href ? `/${locale}${linkHref}` : router.pathname.replace('[locale]', locale);
-    // }
+
     const _channel = channel
         ? channel === DEFAULT_CHANNEL_SLUG && !router.query.locale
             ? ``
@@ -37,7 +35,7 @@ export const Link: React.FC<PropsWithChildren<LinkComponentProps>> = ({
     if (_channel && !skipLocaleHandling) {
         linkHref = href
             ? `${_channel}${locale ? `/${locale}` : ''}${linkHref}`
-            : router.pathname.replace('/[channel]', _channel).replace('/[locale]', `/${locale}` ?? '');
+            : router.pathname.replace('/[channel]', _channel).replace('/[locale]', `/${locale}`);
     }
 
     return (
