b11a9e28393f0c83987d873f436bcbee1566f8c3
florisb <floris@modernmediahub.nl>
2024-12-03 16:30:27 +0100

    Update

--- src/components/molecules/ProductTile.tsx
+++ src/components/molecules/ProductTile.tsx
@@ -1,105 +1,94 @@
 import { Stack, Link, ProductImageGrid, TP } from '@/src/components/atoms/';
 import { CollectionTileType, ProductSearchType } from '@/src/graphql/selectors';
 import { priceFormatter } from '@/src/util/priceFormatter';
+import { Ratings } from './Ratings'; // Assuming Ratings is imported from the same path
 import styled from '@emotion/styled';
 import React from 'react';
 
 export const ProductTile: React.FC<{
     product: ProductSearchType;
-    collections: CollectionTileType[];
     lazy?: boolean;
-}> = ({ product, collections, lazy }) => {
+}> = ({ product, lazy }) => {
     const priceValue =
         'value' in product.priceWithTax
             ? priceFormatter(product.priceWithTax.value, product.currencyCode)
             : product.priceWithTax.min === product.priceWithTax.max
-              ? priceFormatter(product.priceWithTax.min, product.currencyCode)
-              : `${priceFormatter(product.priceWithTax.min, product.currencyCode)} - ${priceFormatter(
+                ? priceFormatter(product.priceWithTax.min, product.currencyCode)
+                : `${priceFormatter(product.priceWithTax.min, product.currencyCode)} - ${priceFormatter(
                     product.priceWithTax.max,
                     product.currencyCode,
                 )}`;
 
+    const src = product.productAsset?.preview;
+    const brand = typeof product.customFields?.brand === 'string' ? product.customFields.brand : undefined;
+
     return (
         <Main column gap="1rem">
-            <Link href={`/products/${product.slug}/`}>
-                <ProductImageGrid
-                    loading={lazy ? 'lazy' : undefined}
-                    src={product.productAsset?.preview}
-                    alt={product.productName}
-                    title={product.productName}
-                />
-            </Link>
-            <Categories gap="0.5rem">
-                {product.collectionIds
-                    .filter((cId, index) => product.collectionIds.indexOf(cId) === index)
-                    .map(cId => collections.find(c => c.id === cId))
-                    .filter(c => c && c.slug !== 'all' && c.slug !== 'search')
-                    .map(c => {
-                        const href =
-                            c?.parent?.slug !== '__root_collection__'
-                                ? `/collections/${c?.parent?.slug}/${c?.slug}`
-                                : `/collections/${c?.slug}`;
-
-                        return (
-                            <CategoryBlock href={href} key={c?.slug}>
-                                <TP
-                                    size="1.25rem"
-                                    color="contrast"
-                                    upperCase
-                                    weight={500}
-                                    style={{ letterSpacing: '0.5px' }}>
-                                    {c?.name}
-                                </TP>
-                            </CategoryBlock>
-                        );
-                    })}
-            </Categories>
-            <Stack column gap="0.25rem">
-                <Stack column gap="0.5rem">
-                    <Link href={`/products/${product.slug}/`}>
+            <Stack style={{ position: 'relative', width: '32rem' }}>
+                <Link href={`/products/${product.slug}/`}>
+                    <ProductImageGrid
+                        loading={lazy ? 'lazy' : undefined}
+                        src={src}
+                        alt={product.productName}
+                        title={product.productName}
+                    />
+                </Link>
+            </Stack>
+            <Stack column gap="0.75rem">
+                <TextWrapper href={`/products/${product.slug}/`}>
+                    <Stack gap="0.5rem">
+                        {brand && (
+                            <Brand size="20px" weight={700} noWrap>
+                                {brand}
+                            </Brand>
+                        )}
                         <ProductName>{product.productName}</ProductName>
-                    </Link>
-                </Stack>
-                <ProductPrice gap="0.25rem">
-                    <ProductPriceValue>{priceValue}</ProductPriceValue>
-                </ProductPrice>
+                    </Stack>
+                    <Stack column gap="0.5rem">
+                        {/* Render review stars */}
+                        <Ratings rating={Math.random() * 5} />
+                    </Stack>
+                    <ProductPrice>
+                        <ProductPriceValue>{priceValue}</ProductPriceValue>
+                    </ProductPrice>
+                </TextWrapper>
             </Stack>
         </Main>
     );
 };
-const Categories = styled(Stack)`
-    position: absolute;
-    top: 0;
-    left: 0;
-    flex-wrap: wrap;
+
+// Styled Components
+const Brand = styled(TP)`
+    font-size: 20px;
+    font-weight: 700;
+    color: ${({ theme }) => theme.text.main};
 `;
 
-const ProductName = styled.div`
+const ProductName = styled(TP)`
     font-weight: 400;
-    color: ${p => p.theme.background.main};
     font-size: 1.5rem;
-`;
-
-const CategoryBlock = styled(Link)`
-    padding: 1rem;
-
-    background-color: ${({ theme }) => theme.tile.background};
-
-    @media (min-width: ${({ theme }) => theme.breakpoints.sm}) {
-        :hover {
-            background-color: ${({ theme }) => theme.gray(500)};
-        }
-    }
+    color: ${({ theme }) => theme.text.main};
 `;
 
 const ProductPrice = styled(Stack)`
     font-size: 1.25rem;
+    font-weight: 500;
 `;
-const ProductPriceValue = styled(Stack)`
+
+const ProductPriceValue = styled(TP)`
     font-weight: 400;
+    font-size: 1.25rem;
+    color: ${({ theme }) => theme.text.main};
 `;
+
+const TextWrapper = styled(Link)`
+    margin-top: 0.75rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+`;
+
 const Main = styled(Stack)`
-    font-size: 1.5rem;
     position: relative;
     width: 100%;
     font-weight: 500;
