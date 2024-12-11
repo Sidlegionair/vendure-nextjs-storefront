92626dbbfaba2ad7ecdeaf3b75ac79dacccdd024
florisb <floris@modernmediahub.nl>
2024-12-11 15:21:11 +0100

    fix inverted about us block

--- src/components/storyblok/AboutUsBlockInverted.js
+++ src/components/storyblok/AboutUsBlockInverted.js
@@ -55,6 +55,12 @@ const AboutUsBlockInverted = ({ blok }) => {
                     //height: auto;
                     color: #fff;
                     overflow: hidden;
+                    @media(max-width: 767px) {
+                        overflow: unset;
+                        height: 887px; /* Fixed height to ensure layout consistency */
+                        padding-bottom: 150px;
+
+                    }
 
                 }
 
@@ -66,6 +72,11 @@ const AboutUsBlockInverted = ({ blok }) => {
                     background-size: cover;
                     //background-position: top center;
                     margin-right: 100px; /* Offset to create the left gap */
+                    @media(max-width: 767px) {
+                        margin-right: 0;
+                        align-items: end;
+
+                    }
                 }
 
                 .about-us-text {
@@ -83,6 +94,15 @@ const AboutUsBlockInverted = ({ blok }) => {
                     background-color: rgba(14, 70, 50, 0.95); /* Semi-transparent background for readability */
                     padding: 82px 192px 82px 82px;
                     z-index: 2;
+
+                    @media (max-width: 767px) {
+                        margin-bottom: -200px;
+                        //margin-bottom: 0;
+                        margin-right: 0;
+                        width: 95vw;
+                        padding: 50px 30px;
+                        height: 440px;
+                    }
                 }
 
                 .about-us-text h2 {
@@ -90,27 +110,40 @@ const AboutUsBlockInverted = ({ blok }) => {
                     font-weight: 600;
                     line-height: 65px;
                     margin-bottom: 1.25rem;
-                }
 
-                .rich-text-content p {
-                    font-family: 'Calibri', sans-serif;
-                    font-size: 20px;
-                    font-weight: 400;
-                    line-height: 26px;
-                    margin-bottom: 1.25rem;
+                    @media (max-width: 767px) {
+                        font-size: 50px;
+                        line-height: 50px;
+                    }
                 }
+                
+                .rich-text-content {
+                    p {
+                        font-family: 'Calibri', sans-serif;
+                        font-size: 20px;
+                        font-weight: 400;
+                        line-height: 26px;
+                        margin-bottom: 1.25rem;
 
-                .learn-more-button {
-                    display: inline-block;
-                    padding: 18px 30px;
-                    background-color: #FFFFFF;
-                    color: #9E2E3A;
-                    border-radius: 8px;
-                    text-decoration: none;
-                    margin-top: 1rem;
-                    font-size: 20px;
-                    font-weight: 600;
-                    line-height: 20px;
+                        @media (max-width: 767px) {
+                            font-size: 18px;
+                            line-height: 26px;
+                        }
+                    }
+                }
+                
+                    .learn-more-button {
+                        display: inline-block;
+                        padding: 18px 30px;
+                        background-color: #FFFFFF;
+                        color: #9E2E3A;
+                        border-radius: 8px;
+                        text-decoration: none;
+                        margin-top: 1rem;
+                        font-size: 20px;
+                        font-weight: 600;
+                        line-height: 20px;
+                    }
                 }
             `}</style>
         </section>
