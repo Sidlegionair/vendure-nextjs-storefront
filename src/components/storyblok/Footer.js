f88e11f3844f423c070cd3790afc9d4314b15f4d
florisb <floris@modernmediahub.nl>
2024-12-16 14:50:11 +0100

    fix footer styling

--- src/components/storyblok/Footer.js
+++ src/components/storyblok/Footer.js
@@ -54,12 +54,21 @@ const Footer = ({ blok }) => {
 
                 {/* Newsletter and Social Links Section */}
                 <div className="newsletter-section">
-                    <input
-                        type="email"
-                        placeholder={newsletter_placeholder}
-                        className="email-input"
-                    />
-                    <button className="send-button">&#9654;</button>
+                    <div className="email-wrapper">
+                        <input
+                            type="email"
+                            placeholder={newsletter_placeholder}
+                            className="email-input"
+                        />
+                        <button className="send-button">
+                            <svg width="24" height="25" viewBox="0 0 24 25" fill="none"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M23.7766 0.337106C23.8824 0.443068 23.9547 0.577778 23.9846 0.724502C24.0145 0.871226 24.0006 1.0235 23.9447 1.1624L15.0525 23.395C14.9741 23.5907 14.8433 23.7611 14.6743 23.8873C14.5054 24.0135 14.3049 24.0907 14.095 24.1103C13.885 24.1299 13.6737 24.0912 13.4843 23.9984C13.295 23.9056 13.1348 23.7624 13.0216 23.5845L8.16521 15.9505L0.532179 11.0935C0.353873 10.9803 0.21028 10.8202 0.117244 10.6306C0.0242089 10.441 -0.0146521 10.2294 0.00494948 10.0191C0.0245511 9.80886 0.101853 9.60809 0.228326 9.43898C0.354799 9.26987 0.525526 9.139 0.721667 9.06079L22.9514 0.170519C23.0903 0.114601 23.2426 0.100724 23.3893 0.130612C23.536 0.1605 23.6707 0.232836 23.7766 0.338635V0.337106ZM9.69029 15.5042L13.9095 22.1341L21.1421 4.05094L9.69029 15.5042ZM20.0617 2.97041L1.98085 10.204L8.61142 14.4222L20.0617 2.97041Z"
+                                    fill="#9E2E3A" />
+                            </svg>
+                        </button>
+                    </div>
                     <div className="social-icons">
                         {social_links.map((social, index) => (
                             <a
@@ -199,35 +208,54 @@ const Footer = ({ blok }) => {
                 }
 
                 .newsletter-section {
-                    //flex: 1 1 250px;
                     display: flex;
                     flex-direction: column;
                     align-items: flex-end;
-                    min-width: 250px;
+                    gap: 15px; /* Space between the input and social icons */
+                    width: 100%;
+                    max-width: 250px;
+                }
+
+                .email-wrapper {
+                    position: relative;
+                    width: 100%;
                 }
 
                 .email-input {
                     background: #ffffff;
-                    padding: 15px 20px;
+                    padding: 15px;
                     font-size: 16px;
                     font-family: 'Calibri', sans-serif;
                     color: #333;
                     border: 1px solid #fff;
-                    margin-bottom: 10px;
                     width: 100%;
                     box-sizing: border-box;
-                    max-width: 300px;
+                    max-width: 100%;
+                    //border-radius: 30px;
+                    padding-right: 50px; /* Space for the button */
                 }
 
                 .send-button {
+                    position: absolute;
+                    right: 10px; /* Inside the input */
+                    top: 50%;
+                    transform: translateY(-50%);
+                    border: none;
+                    //background-color: #c00;
+                    color: #fff;
                     padding: 10px 15px;
                     font-size: 14px;
-                    border-radius: 50%;
-                    border: none;
-                    background-color: #c00;
-                    color: #fff;
+                    //border-radius: 50%;
                     cursor: pointer;
-                    margin-bottom: 20px;
+                }
+
+                .email-input::placeholder {
+                    font-family: 'Calibri', sans-serif;
+                    font-style: normal;
+                    font-weight: 400;
+                    font-size: 16px;
+                    line-height: 24px; /* Identical to box height, or 150% */
+                    color: #4D4D4D;
                 }
 
                 .social-icons {
@@ -246,6 +274,7 @@ const Footer = ({ blok }) => {
                     height: 100%;
                     object-fit: contain;
                 }
+
 
                 @media (max-width: 1024px) {
                     .content-layer {
