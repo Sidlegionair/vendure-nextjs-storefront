a65d04eeb2b51a441737259f590cb4128db86960
florisb <floris@modernmediahub.nl>
2025-02-03 14:24:17 +0100

    Fix hubspot component

--- src/components/storyblok/HubspotFormBlock.js
+++ src/components/storyblok/HubspotFormBlock.js
@@ -3,7 +3,6 @@ import { storyblokEditable } from '@storyblok/react';
 import Script from 'next/script';
 
 const HubspotFormBlock = ({ blok }) => {
-    // Destructure configurable properties with sensible defaults
     const {
         portalId = "145432083",
         formId = "7169da37-5530-45a3-a6f5-13590b81a61c",
@@ -13,29 +12,23 @@ const HubspotFormBlock = ({ blok }) => {
 
     return (
         <section className="hubspot-form-section" {...storyblokEditable(blok)}>
-            {/* This div is where HubSpot will render the form */}
+            {/* Container where HubSpot will render the form */}
             <div id={targetId}></div>
 
-            {/* External HubSpot script */}
+            {/* Load the external HubSpot script */}
             <Script
                 src={`//js-${region}.hsforms.net/forms/embed/v2.js`}
                 strategy="afterInteractive"
-                charset="utf-8"
-                type="text/javascript"
+                onLoad={() => {
+                    if (window.hbspt) {
+                        window.hbspt.forms.create({
+                            portalId,
+                            formId,
+                            target: `#${targetId}`,
+                        });
+                    }
+                }}
             />
-
-            {/* Inline script to create the form */}
-            <Script id="hubspot-inline" strategy="afterInteractive">
-                {`
-          if (window.hbspt) {
-            window.hbspt.forms.create({
-              portalId: "${portalId}",
-              formId: "${formId}",
-              target: "#${targetId}"
-            });
-          }
-        `}
-            </Script>
 
             <style jsx>{`
         .hubspot-form-section {
