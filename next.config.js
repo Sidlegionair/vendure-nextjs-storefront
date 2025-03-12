6e31b4f5782d7afc2d1a05dd43554d565f345f11
florisb <floris@modernmediahub.nl>
2025-03-12 15:00:13 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -23,11 +23,18 @@ const nextConfig = {
     },
     async rewrites() {
         return [
+            // When URL is like /en/nl or /nl/en, the second segment is used.
+            {
+                source: '/:lang(nl|en)/:locale(nl|en)/:path*',
+                destination: `/${DEFAULT_CHANNEL}/:locale/:path*`,
+            },
+            // Fallback for URLs like /nl or /en without a second locale segment.
             {
                 source: '/:locale(nl|en)/:path*',
                 destination: `/${DEFAULT_CHANNEL}/:locale/:path*`,
             },
         ];
+
     },
 
     // i18n: {
